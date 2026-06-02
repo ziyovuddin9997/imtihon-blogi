e530994a40c52953ec3c26e8c263ca2191c95d13
ziyovuddin9997 <ziyovuddinabdullaev9997@gmail.com>
2026-06-02 19:36:43 +0500

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class Like(models.Model):
